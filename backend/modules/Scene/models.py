64b9a304e469d70f0595cf90f8ea30b482280043
Exillite <greencod67@gmail.com>
2023-11-28 02:12:06 +0300

    add scene model

---
+++ backend/modules/Scene/models.py
@@ -0,0 +1,13 @@
+from mangodm import Document
+from modules.Node.models import NodeModel
+
+from typing import Dict, List
+
+
+class SceneModel(Document):
+    title: str
+    nodes: List[NodeModel]
+    data: Dict[str, int]
+
+    class Config:
+        collection_name = "scene"
