61ea3795988033140710fb78f51c719e133bc58b
Exillite <greencod67@gmail.com>
2023-11-28 02:11:06 +0300

    add user models

---
+++ backend/modules/User/models.py
@@ -0,0 +1,14 @@
+from mangodm import Document
+from typing import List
+
+
+class UserModel(Document):
+    nickname: str
+    email: str
+    password: str
+    bio: str
+    role: str
+
+    class Config:
+        collection_name = "user"
+        excludeFieldsResponse = ["password"]
